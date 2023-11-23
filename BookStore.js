7e8ab587526fadc1915a793c780a098a4cd3ec08
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2023-11-23 10:36:44 +0100

    made a sellABook function

--- BookStore.js
+++ BookStore.js
@@ -1,19 +1,35 @@
-let books = [[1, 'Start with why', 'Simon Sinek', 80.0, 13]
+let books = [
+  [1, 'Start with why', 'Simon Sinek', 80.0, 13]
 , [2, 'But how do it know', 'J. Clark Scott', 59.9, 22]
 , [3, 'Clean Code', 'Robert Cecil Martin', 50.0, 5]
 , [4, 'Zero to One', 'Peter Thiel', 45.0, 12]
 , [5, 'You don\'t know JS', 'Kyle Simpson', 39.9, 9]
 ]
 
-addBookToLibrary = (bookId, bookTitle, author, price, quantity) => {
-    let newBook = [bookId, bookTitle, author, price, quantity]
-    books.push(newBook)
-    return newBook
-}
+// for(let i = 0; i<5; i++){
+//     console.log('The Book ID: ', books[i][0])
+//     console.log('The Book Title: ', books[i][1])
+//     console.log('Author: ', books[i][2])
+// }
 
-let addBook = addBookToLibrary(1, 'Start with why', 'Simon Sinek', 80.0, 13)
-let addBook2 = addBookToLibrary(2, 'But how do it know', 'J. Clark Scott', 59.9, 22)
-let addBook3 = addBookToLibrary(3, 'Clean Code', 'Robert Cecil Martin', 50.0, 5)
-let addBook4 = addBookToLibrary(4, 'Zero to One', 'Peter Thiel', 45.0, 12)
-let addBook5 = addBookToLibrary(5, 'You don\'t know JS', 'Kyle Simpson', 39.9, 9)
-console.log(addBook)
+const sellABook = (name, quantity, balance) => {
+    for(let i = 0; i<5; i++){
+        if(name==books[i][1]){
+            if(quantity<=books[i][4]){
+                if(balance>=(books[i][3])*quantity){
+                    console.log('here is your book, Thanks for your purchase!')
+                    books[i][4] = books[i][4] - quantity
+                    balance = balance - (books[i][3]*quantity)
+                } else {
+                    console.log('sorry you have insufficient balance')
+                }
+            } else{
+                console.log('sorry we dont have these many Books')
+            }
+        } else{
+            console.log('we dont have this book in the store')
+        }
+    }
+}
+sellABook('Clean Code', 2, 100)
+sellABook('Clean Code', 1, 100)
